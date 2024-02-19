98f7fca44de02923c40f0eea80115085266cdc79
Nzuzo_Malinga <nzuzomal@gmail.com>
2024-02-20 00:30:32 +0200

    making sure image gallery renders and loads after page refresh

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,10 +1,19 @@
+import { useState, useEffect } from "react";
 import ImageGallery from "react-image-gallery";
-// import stylesheet if you're not already using CSS @import
 import "react-image-gallery/styles/css/image-gallery.css";
 
-export { Page }
+export { Page };
 
 function Page() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoaded(true);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   const images = [
     {
@@ -20,10 +29,22 @@ function Page() {
       thumbnail: "https://picsum.photos/id/1019/250/150/",
     },
   ];
+
   return (
     <>
       <h1>Welcome</h1>
-      <ImageGallery items={images} />;
+
+      {loaded ? (
+        <ImageGallery
+          items={images}
+         
+          onImageLoad={() => {
+            console.log("image loaded");
+          }}
+        />
+      ) : (
+        <div className="gallery loading">Loading...</div>
+      )}
     </>
-  )
+  );
 }
